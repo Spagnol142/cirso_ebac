0dc3b0ccbe660e61bf2d634670af65c1a19d4319
Spagnol142 <130878800+Spagnol142@users.noreply.github.com>
2023-05-08 22:30:14 -0300

    2

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ const containerMensagemSucesso = document.querySelector('.success-message');
 //Retirando o recarregamento automático da página
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    
+
+
     //Obtendo os valores dos campos e convertendo para números inteiros
     const campoAinteiro = parseInt(campoA.value);
     const campoBinteiro = parseInt(campoB.value);
@@ -21,26 +22,18 @@ form.addEventListener('submit', function(e){
         containerMensagemError.innerHTML = errorCampoA;
         containerMensagemError.style.display = 'block';
         campoA.style.border = '1px solid red'
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'none';
     
     } else {
         containerMensagemSucesso.innerHTML = mensagemSucesso;
         containerMensagemSucesso.style.display = 'block';
+        containerMensagemError.style.display = 'none'; // oculta a mensagem de erro
+        campoA.style.border = '1px solid black'; // remove a borda vermelha do campo A
 
         campoA.value = '';
         campoB.value = '';
         
-    }
-});
-
-    campoA.addEventListener('keyup', function(e) {
-    console.log(e.target.value);
-
-    if (campoAinteiro >= campoBinteiro) {
-        campoA.classList.add('error')
-        document.querySelector('.error-messege').style.display = 'block';
-    } else {
-        campoA.classList.remove('error')
-        document.querySelector('.error-messege').style.display = 'none';
     }
 });
 
